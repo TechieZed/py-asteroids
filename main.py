d0e9cd5d9c804e5149b43bfda9efdf60326fddcc
TechieZed <jasz56789@gmail.com>
2025-08-13 16:12:50 -0400

    add while loop and screen fill

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(color=(0,0,0))
 
 
 if __name__ == "__main__":
